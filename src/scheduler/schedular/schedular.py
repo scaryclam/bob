9cf449e8370b7ff4f7d35bb273c0c472d8c84520
Becky Lewis <github@scaryclam.co.uk>
2022-10-27 15:14:02 +0100

    Added basic DB setup

--- src/scheduler/schedular/schedular.py
+++ src/scheduler/schedular/schedular.py
@@ -2,27 +2,36 @@ import sched
 import uuid
 from datetime import datetime
 from time import time, sleep
+import logging
 
 from croniter import croniter
 
 from amqp.publisher import AMQPPublisher
+from models import JobConfig, setup_models
+import settings
+
+
+job_logger = logging.getLogger("job-logger")
 
 
 class Job:
     def __init__(self, job_config):
+        self.job_config = job_config
         self.job_id = str(uuid.uuid4())
-        self.cron_config = job_config['schedule']
-        self.message = job_config['message']
-        self.name = job_config['name']
+        self.cron_config = job_config.schedule
+        self.message = job_config.message
+        self.name = job_config.name
         self.cron = croniter(self.cron_config, datetime.now())
         self.next_schedule = None
+        queue = settings.RABBITMQ_CONFIG['target_queue']
+
         self.publisher = self._make_publisher(
-            job_config['host'],
-            job_config['vhost'],
-            job_config['user'],
-            job_config['password'],
-            job_config['exchange'],
-            job_config['target_queue'])
+            settings.RABBITMQ_CONFIG['host'],
+            settings.RABBITMQ_CONFIG['vhost'],
+            settings.RABBITMQ_CONFIG['user'],
+            settings.RABBITMQ_CONFIG['password'],
+            settings.RABBITMQ_CONFIG['exchange'],
+            queue)
 
     def _make_publisher(self, host, vhost, user, password, exchange, queue):
         publisher = AMQPPublisher(
@@ -40,21 +49,11 @@ class Job:
         return self.next_schedule
 
     def create_config(self):
-        config = {
-            'name': self.name,
-            'message': self.message,
-            'schedule': self.cron_config,
-            'target_queue': 'execute',
-            'vhost': '/',
-            'host': '127.0.0.1',
-            'user': 'manager',
-            'password': 'vagrant',
-            'exchange': 'bob-jobs',
-        }
+        config = self.job_config
         return config
 
     def run(self, scheduler_callback, **kwargs):
-        print(f"Running job {self.job_id} ({self.name})")
+        job_logger.debug(f"Running job {self.job_id} ({self.name})")
         self.publish_job()
         scheduler_callback(self.create_config(), self.job_id)
 
@@ -65,10 +64,6 @@ class Schedular:
     exit = False
 
     def add_job(self, job_config):
-
-        if self.scheduler is None:
-            self.scheduler = sched.scheduler(time, sleep)
-
         job = Job(job_config)
 
         next_run = job.get_next_run_time()
@@ -81,9 +76,15 @@ class Schedular:
             self.jobs[job.job_id] = {'event': event, 'job': job}
 
     def start(self, schedule):
+        setup_models()
+        schedule = JobConfig.select()
+
+        if self.scheduler is None:
+            self.scheduler = sched.scheduler(time, sleep)
+
         # Read the config for things to schedule
         for job in schedule:
-            print(f'Adding {job["name"]}')
+            print(f'Adding {job.name}')
             self.add_job(job)
         self.run_loop()
 
